b14eb5771be780f7d7b49b1501cbbc6bf3419e40
Sapan-Kumar_Singh <spankumar006007@gmail.com>
2023-04-15 02:41:47 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String args[]){
+        RWOnly rw=new RWOnly();
+      //  rw.name="Sapan";
+       // System.out.println(rw.name);
+        rw.setName("Sapan");
+        String name= rw.getName();
+        System.out.println(name);
+    }
 }
